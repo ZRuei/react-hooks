9341d6e9f1e590fe307c77b17e2a6942506aee1f
ZRuei <bigpawsome@gmail.com>
2020-11-21 01:24:10 +0800

    feat: 點按標題展開內容 - 使用 useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,27 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 function Accordion({ items }) {
-  const renderItems = items.map((item) => (
-    <React.Fragment key={item.title}>
-      <div className="title active">
-        <i className="dropdown icon" />
-        {item.title}
-      </div>
-      <div className="content active">
-        <p>{item.content}</p>
-      </div>
-    </React.Fragment>
-  ));
+  const [activeIndex, setActiveIndex] = useState(null);
+  const handleTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+  const renderItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => handleTitleClick(index)}>
+          <i className="dropdown icon" />
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
   return (
     <div className="ui styled accordion">
       {renderItems}
