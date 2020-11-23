b2c000609da24bf8a550319c98d8264cacc7bf59
ZRuei <bigpawsome@gmail.com>
2020-11-23 16:49:49 +0800

    feat: 串接 wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Search() {
   const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const callWikiApi = async () => {
+      await axios.get('https://zh.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+          utf8: '',
+        },
+      });
+    };
+    if (term) {
+      callWikiApi();
+    }
+  }, [term]);
 
   return (
     <div>
